c4a17892c30eaa97a2a5ce52dd1ceae6c14013f6
Belhamida Adil <adil.belhamida@etu.univ-orleans.fr>
2026-05-20 12:41:33 +0200

    Implement Collectionneur class with manager reference

--- sae/src/main/java/fr/univorleans/iut45/briquiuto/Collectionneur.java
+++ sae/src/main/java/fr/univorleans/iut45/briquiuto/Collectionneur.java
@@ -18,3 +18,35 @@ public class Collectionneur implements Utilisateur {
         // Implémentation de l'affichage du menu pour le collectionneur
     }
 }
+
+
+package fr.univorleans.iut45.briquiuto;
+
+import java.util.List;
+
+public class Collectionneur implements Utilisateur {
+
+    // On ajoute une référence vers le manager
+    private BriquesCollectionManager gerer;
+
+    // Constructeur pour initialiser le manager
+    public Collectionneur(BriquesCollectionManager manager) {
+        this.gerer = manager;
+    }
+
+    // La méthode est maintenant très simple : elle demande au manager de faire la recherche
+    public List<Boite> rechercherBoitesParTheme(Theme theme) {
+        return this.gerer.rechercherBoitesParTheme(theme);
+    }
+
+    @Override
+    public void afficherMenu() {
+    }
+
+    public BoitePersonnalisee composerBoitePersonnalisee(String nom, List<Piece> pieces) {
+        return null; // ou return this.gerer.composerBoitePersonnalisee(nom, pieces);
+    }
+
+    public void ajouterCollection(Boite boite) {
+    }
+}
