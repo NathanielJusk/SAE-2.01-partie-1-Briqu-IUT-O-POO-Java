3a56a9470e0310468fe073d9f3d3520ad74a9dd8
zakaria2122 <zakaria.makhlouf45000@gmail.com>
2026-06-10 11:42:55 +0200

    reglage probleme des test Figurine

--- sae/src/test/java/fr/univorleans/iut45/briquiuto/ContenirFTest.java
+++ sae/src/test/java/fr/univorleans/iut45/briquiuto/ContenirFTest.java
@@ -7,22 +7,22 @@ public class ContenirFTest {
     
     @Test
     public void testSetGetFigurine() {
-        ContenirF contenirF = new ContenirF();
-        Figurine figurine = new Figurine(1, "Soldat", 5);
+        ContenirF contenirF = new ContenirF(null, 0);
+        Figurine figurine = new Figurine("1", "Soldat", 5);
         contenirF.setFigurine(figurine);
         assertEquals(figurine, contenirF.getFigurine());
     }
     
     @Test
     public void testSetGetQuantiteF() {
-        ContenirF contenirF = new ContenirF();
+        ContenirF contenirF = new ContenirF(null, 0);
         contenirF.setQuantiteF(2);
         assertEquals(2, contenirF.getQuantiteF());
     }
     
     @Test
     public void testSetFigurineNull() {
-        ContenirF contenirF = new ContenirF();
+        ContenirF contenirF = new ContenirF(null, 0);
         contenirF.setFigurine(null);
         assertNull(contenirF.getFigurine());
     }
--- sae/src/test/java/fr/univorleans/iut45/briquiuto/FigurinTest.java
+++ sae/src/test/java/fr/univorleans/iut45/briquiuto/FigurinTest.java
@@ -7,29 +7,29 @@ public class FigurinTest {
     
     @Test
     public void testConstructeur() {
-        Figurine figurine = new Figurine(1, "Chevalier", 3);
+        Figurine figurine = new Figurine("1", "Chevalier", 3);
         assertEquals("Chevalier", figurine.getNomFig());
-        assertEquals(1, figurine.getIdFig());
+        assertEquals("1", figurine.getIdFig());
         assertEquals(3, figurine.getNbParties());
     }
     
     @Test
     public void testSetGetIdFig() {
-        Figurine figurine = new Figurine(1, "Chevalier", 3);
-        figurine.setIdFig(2);
-        assertEquals(2, figurine.getIdFig());
+        Figurine figurine = new Figurine("1", "Chevalier", 3);
+        figurine.setIdFig("2");
+        assertEquals("2", figurine.getIdFig());
     }
     
     @Test
     public void testSetGetNomFig() {
-        Figurine figurine = new Figurine(1, "Chevalier", 3);
+        Figurine figurine = new Figurine("1", "Chevalier", 3);
         figurine.setNomFig("Dragon");
         assertEquals("Dragon", figurine.getNomFig());
     }
     
     @Test
     public void testSetGetNbParties() {
-        Figurine figurine = new Figurine(1, "Chevalier", 3);
+        Figurine figurine = new Figurine("1", "Chevalier", 3);
         figurine.setNbParties(5);
         assertEquals(5, figurine.getNbParties());
     }
