c9bea83aeaaf412328e8759e56304039cb21dbec
zakaria2122 <zakaria.makhlouf45000@gmail.com>
2026-06-15 14:43:48 +0200

    choix entre Console ou interface pour la compilation

--- compilation.sh
+++ compilation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ============================================================
-#  Script de compilation et lancement (Version Terminal via Hack)
+#  Script de compilation et lancement (Multi-versions)
 #  Projet Briqu'IUT-O — SAÉ 2.01
 # ============================================================
 
@@ -21,10 +21,33 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 echo "Compilation OK"
-
 echo ""
-echo "=== Lancement de l'application (Terminal) ==="
 
-# mvn javafx:run -Djavafx.mainClass="fr.univorleans.iut45.briquiuto.App" -q
+# ---------------------------------------------------------
+# MENU DE CHOIX
+# ---------------------------------------------------------
+echo "================================================="
+echo "   Quelle version de l'application lancer ?"
+echo "   1) Version Terminal (App.java)"
+echo "   2) Version Graphique (ApplicationJavaFx.java)"
+echo "================================================="
+read -p "Entrez votre choix (1 ou 2) : " CHOIX
+echo ""
 
-mvn javafx:run -Djavafx.mainClass="fr.univorleans.iut45.briquiuto.AppJavaFx" -q
+# ---------------------------------------------------------
+# EXECUTION SELON LE CHOIX
+# ---------------------------------------------------------
+if [ "$CHOIX" = "1" ]; then
+    echo "=== Lancement de l'application (TERMINAL) ==="
+    # On contourne Maven ! On utilise Java pur en lui donnant le chemin 
+    # de ton code compilé et de la bibliothèque MariaDB.
+    java -cp "target/classes:$HOME/.m2/repository/org/mariadb/jdbc/mariadb-java-client/3.3.2/mariadb-java-client-3.3.2.jar" fr.univorleans.iut45.briquiuto.App
+
+elif [ "$CHOIX" = "2" ]; then
+    echo "=== Lancement de l'application (GRAPHIQUE) ==="
+    mvn javafx:run -q
+
+else
+    echo "Choix invalide. Le lancement a été annulé."
+    exit 1
+fi
