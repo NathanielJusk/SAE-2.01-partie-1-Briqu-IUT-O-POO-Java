d380b5a81612d0f04612312adbc78445c3ab3374
MouradBouslim <mourad.bouslim@etu.univ-orleans.fr>
2026-06-16 17:05:00 +0200

    implementation du controleur accueil

--- sae/src/main/java/fr/univorleans/iut45/briquiuto/IHM/Controlleurs/AccueilControleur.java
+++ sae/src/main/java/fr/univorleans/iut45/briquiuto/IHM/Controlleurs/AccueilControleur.java
@@ -22,14 +22,14 @@ public class AccueilControleur {
     }
 
     private void initialiser() {
-        // Les deux boutons ouvrent maintenant la même page de connexion !
+        // Les deux boutons ouvrent maintenant la mÃªme page de connexion !
         this.vue.getBtnCollection().setOnAction(event -> ouvrirPageConnexion());
         this.vue.getBtnAdmin().setOnAction(event -> ouvrirPageConnexion());
         
         this.vue.getBtnQuit().setOnAction(event -> actionQuitter());
     }
 
-    // Méthode commune pour ouvrir le formulaire de connexion
+    // MÃ©thode commune pour ouvrir le formulaire de connexion
     private void ouvrirPageConnexion() {
         System.out.println("Ouverture de la page de connexion...");
         
@@ -41,7 +41,7 @@ public class AccueilControleur {
     }
 
     private void actionQuitter() {
-        System.out.println("Fermeture de l'application demandée.");
+        System.out.println("Fermeture de l'application demandÃ©e.");
         Platform.exit();
     }
 }
